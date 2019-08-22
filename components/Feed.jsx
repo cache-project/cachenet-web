cd8f5704461b8fea745026ac73121a5cb468957d
Piper McCorkle <contact@piperswe.me>
2019-08-22 13:32:44 -0700

    style(eslint): :lipstick: fix linting issues
    undefined

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 import styles from './Feed.module.scss';
 
 export default function Feed() {
-  return <div className={styles.feed}>
-    Feed <Link to="/">goto landing</Link>
-  </div>;
+  return (
+    <div className={styles.feed}>
+      Feed
+      <Link to="/">goto landing</Link>
+    </div>
+  );
 }
