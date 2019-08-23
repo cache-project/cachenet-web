aacf43f2be7b58a3ccb78af21d0e95ce69efefeb
Piper McCorkle <contact@piperswe.me>
2019-08-22 22:18:10 -0700

    feat(app): :sparkles: use BrowserRouter instead of HashRouter
    undefined
    
    BREAKING CHANGE: "#/" should be removed from the beginning of paths

--- components/App.jsx
+++ components/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import loadable from '@loadable/component';
 
 import Landing from './Landing';
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Route path="/" exact component={Landing} />
       <Route path="/feed" component={loadable(() => import('./Feed'))} />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -158,5 +158,6 @@ module.exports = {
     } : {},
   },
   devServer: {
+    historyApiFallback: true,
   },
 };
